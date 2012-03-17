8f21765fc170197af57403f54be1ee8396e3b3ec
Jason Gonzales <jason.gonzales23@gmail.com>
2012-03-16 21:00:28 -0400

    the whole shebang

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+function unlock(el){
+    this.dragger = document.querySelector(el);
+    //var dragger = this.dragger; //will this help?
+    this.draggerWidth = this.dragger.offsetWidth + 2; //add 2px to have it sit in the track nicer
+    this.dragger.style.webkitTransitionProperty = '-webkit-transform';
+    this.dragger.style.webkitTransform = 'translate3d(0px, 0, 0)';
+    
+    this.start = function(e){
+        this.startDelta = e.touches ? e.touches[0].pageX : e.pageX;
+        this.delta=0;
+        this.containerWidth = this.dragger.parentNode.offsetWidth - this.draggerWidth;
+        document.addEventListener('mousemove', this);
+        document.addEventListener('mouseup', this);
+        this.dragger.addEventListener('touchmove', this);
+        this.dragger.addEventListener('touchend', this);
+    };
+    
+    this.move = function(e){
+        this.delta = (e.touches ? e.touches[0].pageX : e.pageX) - this.startDelta;
+       
+        if (this.delta < 0 ){ this.delta = 0;}
+        if (this.delta > this.containerWidth){
+            this.delta = this.containerWidth;
+        }
+        this.dragger.style.webkitTransitionDuration = '0ms';
+        this.dragger.style.webkitTransform = 'translate3d(' + this.delta + 'px, 0, 0)';
+    }
+    
+    this.end = function(e){
+        if(this.delta === this.containerWidth){
+            console.log('success');
+        }
+        this.dragger.style.webkitTransitionDuration = '400ms';
+        this.dragger.style.webkitTransform = 'translate3d(0px, 0, 0)';
+        document.removeEventListener('mousemove', this);
+        document.removeEventListener('mouseup', this);        
+    }
+    
+    this.dragger.addEventListener('mousedown', this);
+    this.dragger.addEventListener('touchstart', this);   
+}
+
+unlock.prototype = {
+    handleEvent: function(e){
+        switch(e.type) {
+            case'mousedown':
+                this.start(e);
+                break;
+            case'mousemove':
+                this.move(e);
+                break;
+            case'mouseup':
+                this.end(e);
+                break;
+            case'touchstart':
+                this.start(e);
+                break;
+            case'touchmove':
+                this.move(e);
+                break;
+            case'touchend':
+                this.end(e);
+                break;
+        }
+    }
+};
+
+var myUnlock = new unlock('#one');
+var myUnlock2 = new unlock('#two');
